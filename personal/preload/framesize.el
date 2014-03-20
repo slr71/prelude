4b5a7068501e7092d984d51cb5d651e8bbf0b0a8
Dennis Roberts <dennis@iplantcollaborative.org>
2014-03-19 17:11:34 -0700

    added a package to set the default frame size

--- personal/preload/framesize.el
+++ personal/preload/framesize.el
@@ -8,8 +8,8 @@
 ;; Set the initial frame size.
 (add-to-list 'default-frame-alist '(top . 25))
 (add-to-list 'default-frame-alist '(left . 0))
-(add-to-list 'default-frame-alist '(height . 60))
-(add-to-list 'default-frame-alist '(width . 120))
+(add-to-list 'default-frame-alist '(height . 81))
+(add-to-list 'default-frame-alist '(width . 100))
 
 (provide 'framesize)
 ;;; framesize.el ends here
