7b1605d8a4a32d8f43d0684ca4ecae0baca4fde0
Dennis Roberts <dennis@iplantcollaborative.org>
2014-03-19 17:40:38 -0700

    added a package for personal editor preferences

---
+++ personal/editor-prefs.el
@@ -0,0 +1,14 @@
+;;; editor-prefs.el --- my editor preferences.
+;;;
+;;; Commentary:
+;;;
+;;; Configures the editor to my tastes.
+;;;
+;;; Code:
+
+;;; Set the line length to iPlant's standard for Clojure.
+(require 'whitespace)
+(setq whitespace-line-column 100)
+
+(provide 'editor-prefs)
+;;; editor-prefs.el ends here
