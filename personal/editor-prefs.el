1fda3bcf4848cd0fbf243e996a2fc5fa606a125e
Dennis Roberts <dennis@iplantcollaborative.org>
2015-11-30 16:34:35 -0700

    added a function to unfill a paragraph or region

--- personal/editor-prefs.el
+++ personal/editor-prefs.el
@@ -41,5 +41,16 @@
 ;;; The audible and visible bells are both kind of annoying; disable them.
 (setq ring-bell-function 'ignore)
 
+;;; A function to unfill a paragraph.
+(defun unfill-paragraph (&optional region)
+  "Convert the REGION or a multi-line paragraph into a single line of text."
+  (interactive (progn (barf-if-buffer-read-only) '(t)))
+  (let ((fill-column (point-max))
+        (emacs-lisp-docstring-fill-column t))
+    (fill-paragraph nil region)))
+
+;;; A key binding for the unfill-paragraph function.
+(define-key global-map "\M-Q" 'unfill-paragraph)
+
 (provide 'editor-prefs)
 ;;; editor-prefs.el ends here
