bb886ee92f77b22274d51477458407f9be676aa5
Dennis Roberts <dennis@cyverse.org>
2017-01-03 10:14:15 -0700

    made the frame size customizable for each machine

--- personal/preload/framesize.el
+++ personal/preload/framesize.el
@@ -5,11 +5,20 @@
 ;;; I use this to place the frame where I like it.
 ;;;
 ;;; Code:
+
+(defun set-default-initial-frame-size ()
+  "Set the initial frame size to the default values."
+  (add-to-list 'default-frame-alist '(top . 25))
+  (add-to-list 'default-frame-alist '(left . 0))
+  (add-to-list 'default-frame-alist '(height . 95))
+  (add-to-list 'default-frame-alist '(width . 120)))
+
 ;; Set the initial frame size.
-(add-to-list 'default-frame-alist '(top . 25))
-(add-to-list 'default-frame-alist '(left . 0))
-(add-to-list 'default-frame-alist '(height . 95))
-(add-to-list 'default-frame-alist '(width . 120))
+(let ((filename (expand-file-name "~/.framesize.el")))
+  (message filename)
+  (if (file-exists-p filename)
+      (load-file filename)
+    (set-default-initial-frame-size)))
 
 (provide 'framesize)
 ;;; framesize.el ends here
