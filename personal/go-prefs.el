3a4e04292c97b6db0573f798f5fbf04de6061f45
Sarah Roberts <sarahr@cyverse.org>
2019-10-02 14:45:41 -0700

    added some handy Go keybindings

--- personal/go-prefs.el
+++ personal/go-prefs.el
@@ -6,11 +6,22 @@
 ;;;
 ;;; Code:
 
-;;; Set the tab width to 2 characters.
-add-hook 'go-mode-hook
-          '(lambda ()
-             (setq-default)
-             (setq tab-width 2)))
+defun my-go-mode-hook ()
+  (setq tab-width 2)
+
+  ;; guru settings
+  (go-guru-hl-identifier-mode)
+
+  ;; Key bindings specific to go-mode
+  (local-set-key (kbd "M-.") 'godef-jump)
+  (local-set-key (kbd "M-*") 'pop-tag-mark)
+  (local-set-key (kbd "M-p") 'compile)
+  (local-set-key (kbd "M-P") 'recompile)
+  (local-set-key (kbd "M-]") 'next-error)
+  (local-set-key (kbd "M-[") 'previous-error))
+
+;;; Add my custom hooks.
+(add-hook 'go-mode-hook 'my-go-mode-hook)
 
 ;;; Explicitly set the GOPATH environment variable.
 (add-hook 'go-mode-hook
