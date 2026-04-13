5dc37b86b6991e2f84ec891354d2210286b6aba3
Sarah Roberts <sonoran.sarah@gmail.com>
2026-04-13 12:47:55 -0700

    moved some state files to other locations and added jdtls caching for Java projects

--- personal/editor-prefs.el
+++ personal/editor-prefs.el
@@ -10,6 +10,9 @@
 (require 'whitespace)
 (setq whitespace-line-column 120)
 
+;;; Ensure that this directory is in the load path.
+(add-to-list 'load-path (file-name-directory load-file-name))
+
 ;;; Set the default fill column.
 (setq-default fill-column 120)
 
@@ -38,6 +41,13 @@
 (prelude-require-packages '(just-ts-mode))
 (require 'just-ts-mode)
 (just-ts-mode-install-grammar)
+
+;;; Use the ~/.emacs.d/var for storing state files.
+(require 'utils)
+(let ((var-dir (emacs-subdir-path "var")))
+  (unless (file-exists-p var-dir)
+    (make-directory var-dir t))
+  (setq project-list-file (expand-file-name "projects" var-dir)))
 
 (provide 'editor-prefs)
 ;;; editor-prefs.el ends here
--- personal/prelude-java.el
+++ personal/prelude-java.el
@@ -24,6 +24,20 @@
 (add-hook 'java-mode-hook (lambda () (run-hooks 'prelude-java-mode-hook)))
 (add-hook 'java-ts-mode-hook (lambda () (run-hooks 'prelude-java-mode-hook)))
 
+;; Configure jdtls to use a cache directory for faster startup times.
+(require 'utils)
+(with-eval-after-load 'eglot
+  (defun prelude-jdtls-contact (_interactive-call)
+    "Return the jdtls command with a persistent workspace directory."
+    (let* ((project (project-current))
+           (project-name (if project (file-name-nondirectory (directory-file-name (project-root project))) "default"))
+           (workspace-dir (emacs-subdir-path ".cache" "jdtls" project-name)))
+      (unless (file-exists-p workspace-dir)
+        (make-directory workspace-dir t))
+      (list "jdtls" "-data" workspace-dir)))
+  (add-to-list 'eglot-server-programs
+               '((java-mode java-ts-mode) . prelude-jdtls-contact)))
+
 (provide 'prelude-java)
 
 ;;; prelude-java.el ends here
