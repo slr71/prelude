03fdcd50cbe9bc1e32c727739b0eae9eb58e30ca
Sarah Roberts <sonoran.sarah@gmail.com>
2026-07-21 13:47:48 -0700

    added some org-mode preferences

---
+++ personal/org-prefs.el
@@ -0,0 +1,16 @@
+;;; -*- lexical-binding: nil -*-
+;;;
+;;; org-prefs.el --- my org-mode preferences.
+;;;
+;;; Commentary:
+;;;
+;;; Configures org-mode to my tastes.
+;;;
+;;; Code:
+
+(require 'org)
+
+(setq org-agenda-files '("~/src/tasks/work.org"))
+
+(provide 'org-prefs)
+;;; org-prefs.el ends here
